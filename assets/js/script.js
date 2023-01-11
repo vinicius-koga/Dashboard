d82463a779766010b281ba6681c89e007936b98d
Vinicius Koga <viniciuskoga_m@hotmail.com>
2023-01-11 02:58:05 -0300

    Added localStorage theme function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 function switchTheme() {
     let checkbox = document.querySelector('.switchThemeBtn');
     if (checkbox.classList.contains('unselected')) {
+        localStorage.setItem('Theme', 'dark');
+
         checkbox.classList.remove('unselected');
         checkbox.classList.add('selected');
 
@@ -30,6 +32,8 @@ function switchTheme() {
         })
 
     } else if (checkbox.classList.contains('selected')) {
+        localStorage.setItem('Theme', '');
+
         checkbox.classList.remove('selected');
         checkbox.classList.add('unselected');
 
@@ -60,3 +64,39 @@ function switchTheme() {
 
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    let theme = localStorage.getItem('Theme');
+    let checkbox = document.querySelector('.switchThemeBtn');
+    if (theme) {
+        checkbox.setAttribute('checked', '');
+
+        checkbox.classList.remove('unselected');
+        checkbox.classList.add('selected');
+
+        document.querySelectorAll('.lightTheme').forEach((item) => {
+            item.classList.remove('lightTheme');
+            item.classList.add('darkTheme');
+        })
+
+        document.querySelectorAll('.fontLightTheme').forEach((item) => {
+            item.classList.remove('fontLightTheme');
+            item.classList.add('fontDarkTheme');
+        })
+
+        document.querySelector('.dashboard').classList.remove('dashboardLight');
+        document.querySelector('.dashboard').classList.add('dashboardDark');
+
+        document.querySelector('.sidebar').style.borderRight = '1px solid #111';
+
+        document.querySelectorAll('.accordionLight').forEach((item) => {
+            item.classList.remove('accordionLight');
+            item.classList.add('accordionDark');
+        })
+
+        document.querySelectorAll('.accordionBtnLight').forEach((item) => {
+            item.classList.remove('accordionBtnLight');
+            item.classList.add('accordionBtnDark')
+        })
+    }
+})
