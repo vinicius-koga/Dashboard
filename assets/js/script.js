02b7b7cfb177538005685199e7c9440fed265e34
Vinicius Koga <viniciuskoga_m@hotmail.com>
2023-01-12 20:18:01 -0300

    Sidebar finished, started dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,11 @@ function switchTheme() {
             item.classList.add('darkTheme');
         })
 
+        document.querySelectorAll('.dashboardFontLight').forEach((item) => {
+            item.classList.remove('dashboardFontLight');
+            item.classList.add('dashboardFontDark');
+        })
+
         document.querySelectorAll('.fontLightTheme').forEach((item) => {
             item.classList.remove('fontLightTheme');
             item.classList.add('fontDarkTheme');
@@ -40,6 +45,11 @@ function switchTheme() {
         document.querySelectorAll('.darkTheme').forEach((item) => {
             item.classList.remove('darkTheme');
             item.classList.add('lightTheme');
+        })
+
+        document.querySelectorAll('.dashboardFontDark').forEach((item) => {
+            item.classList.remove('dashboardFontDark');
+            item.classList.add('dashboardFontLight');
         })
 
         document.querySelectorAll('.fontDarkTheme').forEach((item) => {
@@ -79,6 +89,11 @@ document.addEventListener('DOMContentLoaded', () => {
             item.classList.add('darkTheme');
         })
 
+        document.querySelectorAll('.dashboardFontLight').forEach((item) => {
+            item.classList.remove('dashboardFontLight');
+            item.classList.add('dashboardFontDark');
+        })
+
         document.querySelectorAll('.fontLightTheme').forEach((item) => {
             item.classList.remove('fontLightTheme');
             item.classList.add('fontDarkTheme');
@@ -99,4 +114,38 @@ document.addEventListener('DOMContentLoaded', () => {
             item.classList.add('accordionBtnDark')
         })
     }
+})
+
+const menuOp = document.querySelector('.menuOpener');
+const sidebar = document.querySelector('.sidebar');
+const closeBtn = document.querySelector('.sideBarCloseBtn');
+const backDrop = document.querySelector('.backDrop');
+
+menuOp.addEventListener('click', () => {
+    let sidebarPos = sidebar.getBoundingClientRect();
+    if (sidebarPos.left === -300) {
+        sidebar.style.marginLeft = '0';
+        menuOp.style.opacity = '0';
+        menuOp.style.cursor = 'auto'
+        backDrop.style.display = 'block';
+        setTimeout(() => {
+            backDrop.style.opacity = '0.5';
+        })
+    }
+})
+
+closeBtn.addEventListener('click', () => {
+    if (sidebar.style.marginLeft === '0px') {
+        sidebar.style.removeProperty('margin-left');
+        menuOp.style.opacity = '1';
+        menuOp.style.cursor = 'pointer'
+    }
+})
+
+backDrop.addEventListener('click', () => {
+    sidebar.style.removeProperty('margin-left');
+    menuOp.style.opacity = '1';
+    menuOp.style.cursor = 'pointer'
+    backDrop.style.opacity = '0';
+    backDrop.style.display = 'none';
 })
